a645d5a970bf0c0b86feb7e7e438349cbc3e95cf
zhenya457 <derkachzhenya>
2024-06-21 17:33:56 +0300

    v3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('post', [PostController::class, 'index'])->name('post.index');
